cf10600ff54835ccfbcf45cff2d9fe79d8b59a54
soo1e <gameswuon@gmail.com>
2025-01-04 22:07:18 +0900

    Blank, Null 처리

--- com.templlo.service.promotion/src/main/java/com/templlo/service/promotion/dto/PromotionRequestDto.java
+++ com.templlo.service.promotion/src/main/java/com/templlo/service/promotion/dto/PromotionRequestDto.java
@@ -1,17 +1,35 @@
 package com.templlo.service.promotion.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 
 import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor
 public class PromotionRequestDto {
+
+    @NotBlank(message = "프로모션 이름은 필수입니다.")
     private String name;
+
+    @NotBlank(message = "프로모션 유형은 필수입니다.")
     private String type; // HOTDEAL, MEMBERSHIP 등
+
+    @NotNull(message = "시작일은 필수입니다.")
     private LocalDate startDate;
+
+    @NotNull(message = "종료일은 필수입니다.")
     private LocalDate endDate;
-    private String couponType;
-    private Integer totalCoupons;
+
+    @NotBlank(message = "쿠폰 유형은 필수입니다.")
+    private String couponType; // 할인, 입장권 등
+
+    private Integer maleCoupon;
+    private Integer femaleCoupon;
+
+    @NotNull(message = "전체 쿠폰 수량은 필수입니다.")
+    private Integer totalCoupon;
 }
