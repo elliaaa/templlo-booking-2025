ac0daf12f3fd08dbce1e2acd773d48592014bb65
soo1e <gameswuon@gmail.com>
2025-01-04 22:08:02 +0900

    업데이트 구현

--- com.templlo.service.promotion/src/main/java/com/templlo/service/promotion/controller/PromotionController.java
+++ com.templlo.service.promotion/src/main/java/com/templlo/service/promotion/controller/PromotionController.java
@@ -2,10 +2,14 @@ package com.templlo.service.promotion.controller;
 
 import com.templlo.service.promotion.dto.PromotionRequestDto;
 import com.templlo.service.promotion.dto.PromotionResponseDto;
+import com.templlo.service.promotion.dto.PromotionUpdateDto;
 import com.templlo.service.promotion.service.PromotionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/promotion")
@@ -15,8 +19,16 @@ public class PromotionController {
     private final PromotionService promotionService;
 
     @PostMapping
-    public ResponseEntity<PromotionResponseDto> createPromotion(@RequestBody PromotionRequestDto requestDto) {
+    public ResponseEntity<PromotionResponseDto> createPromotion(@Valid @RequestBody PromotionRequestDto requestDto) {
         PromotionResponseDto responseDto = promotionService.createPromotion(requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PatchMapping("/{promotionId}")
+    public ResponseEntity<PromotionResponseDto> updatePromotion(
+            @PathVariable UUID promotionId,
+            @RequestBody PromotionUpdateDto updateDto) {
+        PromotionResponseDto responseDto = promotionService.updatePromotion(promotionId, updateDto);
         return ResponseEntity.ok(responseDto);
     }
 }
