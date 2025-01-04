e3d617268d644d2eb7ac1e6aa6e8392287a59a4e
soo1e <gameswuon@gmail.com>
2025-01-04 22:46:55 +0900

    전체 목록 조회 쿼리문 작성

--- com.templlo.service.promotion/src/main/java/com/templlo/service/promotion/repository/PromotionRepository.java
+++ com.templlo.service.promotion/src/main/java/com/templlo/service/promotion/repository/PromotionRepository.java
@@ -1,11 +1,21 @@
 package com.templlo.service.promotion.repository;
 
 import com.templlo.service.promotion.entity.Promotion;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface PromotionRepository extends JpaRepository<Promotion, UUID> {
+    @Query("SELECT p FROM Promotion p " +
+            "WHERE (:type IS NULL OR p.type = :type) " +
+            "AND (:status IS NULL OR p.status = :status)")
+    Page<Promotion> findByFilters(@Param("type") String type,
+                                  @Param("status") String status,
+                                  Pageable pageable);
 }
