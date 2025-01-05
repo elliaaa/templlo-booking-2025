649689288c52052d571ecb5133935b6f32ee765b
soo1e <gameswuon@gmail.com>
2025-01-05 15:50:22 +0900

    서비스 계층에서 DTO : Class -> Record로 인한 Builder에서 생성자로 변경

--- com.templlo.service.promotion/src/main/java/com/templlo/service/promotion/dto/PromotionUpdateDto.java
+++ com.templlo.service.promotion/src/main/java/com/templlo/service/promotion/dto/PromotionUpdateDto.java
@@ -2,35 +2,30 @@ package com.templlo.service.promotion.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@Getter
-@NoArgsConstructor
-public class PromotionUpdateDto {
+public record PromotionUpdateDto(
+        @NotBlank(message = "프로모션 이름은 필수입니다.")
+        String name, // 프로모션 이름
 
-    @NotBlank(message = "프로모션 이름은 필수입니다.")
-    private String name; // 프로모션 이름
+        @NotNull(message = "시작일은 필수입니다.")
+        LocalDate startDate, // 시작일
 
-    @NotNull(message = "시작일은 필수입니다.")
-    private LocalDate startDate; // 시작일
+        @NotNull(message = "종료일은 필수입니다.")
+        LocalDate endDate, // 종료일
 
-    @NotNull(message = "종료일은 필수입니다.")
-    private LocalDate endDate; // 종료일
+        @NotNull(message = "남성 쿠폰 수량은 필수입니다.")
+        Integer maleCoupons, // 남성 쿠폰 수량
 
-    @NotNull(message = "남성 쿠폰 수량은 필수입니다.")
-    private Integer maleCoupons; // 남성 쿠폰 수량
+        @NotNull(message = "여성 쿠폰 수량은 필수입니다.")
+        Integer femaleCoupons, // 여성 쿠폰 수량
 
-    @NotNull(message = "여성 쿠폰 수량은 필수입니다.")
-    private Integer femaleCoupons; // 여성 쿠폰 수량
+        @NotNull(message = "전체 쿠폰 수량은 필수입니다.")
+        Integer totalCoupons, // 전체 쿠폰 수량
 
-    @NotNull(message = "전체 쿠폰 수량은 필수입니다.")
-    private Integer totalCoupons; // 전체 쿠폰 수량
+        @NotBlank(message = "쿠폰 유형은 필수입니다.")
+        String couponType, // 쿠폰 유형 (예: DISCOUNT, ENTRY 등)
 
-    @NotBlank(message = "쿠폰 유형은 필수입니다.")
-    private String couponType; // 쿠폰 유형 (예: DISCOUNT, ENTRY 등)
-
-    private String status; // 프로모션 상태
-}
+        String status // 프로모션 상태
+) {}
