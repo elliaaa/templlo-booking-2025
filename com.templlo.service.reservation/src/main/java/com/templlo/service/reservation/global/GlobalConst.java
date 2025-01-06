5b4498c9e18f537e0dae128c230bbf3cc4199a4c
jeeheaG <jeehea99@naver.com>
2025-01-07 00:50:26 +0900

    :sparkles: add : make authentication filter

---
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/global/GlobalConst.java
@@ -0,0 +1,8 @@
+package com.templlo.service.reservation.global;
+
+public class GlobalConst {
+    // TODO : 인증 정보 헤더값 임시
+    public static final String HEADER_NAME_USER_ID = "USER_ID";
+    public static final String HEADER_NAME_USER_ROLE = "USER_ROLE";
+    public static final String HEADER_NAME_USED_AUTH_TOKEN = "JWT_TOKEN";
+}
