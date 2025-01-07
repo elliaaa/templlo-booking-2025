1bc6938ebefbdd4028a6063fb41933db45f4d6c3
jeeheaG <jeehea99@naver.com>
2025-01-07 14:22:31 +0900

    :sparkles: edit : edit auth header name and time zone id

--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/global/GlobalConst.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/global/GlobalConst.java
@@ -1,8 +1,9 @@
 package com.templlo.service.reservation.global;
 
 public class GlobalConst {
-    // TODO : 인증 정보 헤더값 임시
-    public static final String HEADER_NAME_USER_ID = "USER_ID";
-    public static final String HEADER_NAME_USER_ROLE = "USER_ROLE";
-    public static final String HEADER_NAME_USED_AUTH_TOKEN = "JWT_TOKEN";
+    public static final String HEADER_NAME_USER_ID = "X-Login-Id";
+    public static final String HEADER_NAME_USER_ROLE = "X-User-Role";
+    public static final String HEADER_NAME_USED_AUTH_TOKEN = "X-Token";
+
+    public static final String TIME_ZONE_ID = "Asia/Seoul";
 }
