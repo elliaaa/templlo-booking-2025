49958915976e465a0e70a2227d5d18eefa68324d
jeeheaG <jeehea99@naver.com>
2025-01-07 14:59:58 +0900

    :fire: remove : remove useless const

--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/global/GlobalConst.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/global/GlobalConst.java
@@ -4,6 +4,4 @@ public class GlobalConst {
     public static final String HEADER_NAME_USER_ID = "X-Login-Id";
     public static final String HEADER_NAME_USER_ROLE = "X-User-Role";
     public static final String HEADER_NAME_USED_AUTH_TOKEN = "X-Token";
-
-    public static final String TIME_ZONE_ID = "Asia/Seoul";
 }
