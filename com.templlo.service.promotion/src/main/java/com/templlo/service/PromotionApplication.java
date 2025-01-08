f2a675717ce9c9d0dd386d495283054af2738435
soo1e <gameswuon@gmail.com>
2025-01-08 10:46:58 +0900

    :sparkles: add : 캐싱 어노테이션 추가

--- com.templlo.service.promotion/src/main/java/com/templlo/service/PromotionApplication.java
+++ com.templlo.service.promotion/src/main/java/com/templlo/service/PromotionApplication.java
@@ -1,9 +1,11 @@
-package com.templlo.service.promotion;
+package com.templlo.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PromotionApplication {
 
 	public static void main(String[] args) {
