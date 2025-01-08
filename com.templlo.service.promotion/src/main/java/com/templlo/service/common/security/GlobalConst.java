32a3ea844a910d7f9b64add9b8cb1d1cea4aff95
soo1e <gameswuon@gmail.com>
2025-01-08 10:50:33 +0900

    :sparkles: add : common.security

---
+++ com.templlo.service.promotion/src/main/java/com/templlo/service/common/security/GlobalConst.java
@@ -0,0 +1,7 @@
+package com.templlo.service.common.security;
+
+public class GlobalConst {
+	public static final String HEADER_NAME_USER_ID = "X-Login-Id";
+	public static final String HEADER_NAME_USER_ROLE = "X-User-Role";
+	public static final String HEADER_NAME_USED_AUTH_TOKEN = "X-Token";
+}
---
+++ com.templlo.service.promotion/src/main/java/com/templlo/service/common/security/UserRole.java
@@ -0,0 +1,7 @@
+package com.templlo.service.common.security;
+
+public enum UserRole {
+	USER,
+	TEMPLE_ADMIN,
+	MASTER
+}
