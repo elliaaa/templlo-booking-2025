93dd4f2492557dc9f7ba15626667a47e94032cf7
jeeheaG <jeehea99@naver.com>
2025-01-08 16:37:20 +0900

    :sparkles: add : make audit aware config

---
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/global/security/AuditorAwareImpl.java
@@ -0,0 +1,17 @@
+package com.templlo.service.reservation.global.security;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return Optional.of(authentication.getName());
+    }
+}
