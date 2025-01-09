3495dff9b61c673cc298881bdfd06cadcebcce3b
jeeheaG <jeehea99@naver.com>
2025-01-10 01:41:38 +0900

    :sparkles: add : make convert method paged model to page response

--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/global/common/response/PageResponse.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/global/common/response/PageResponse.java
@@ -1,19 +1,31 @@
 package com.templlo.service.reservation.global.common.response;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.web.PagedModel;
 
 import java.util.List;
 
 public record PageResponse<T>(
-        int totalPages,
+        int totalPage,
         int pageNumber,
+        int size,
         List<T> content
 ) {
     public static <T> PageResponse<T> of(Page<T> page) {
         return new PageResponse<>(
                 page.getTotalPages(),
                 page.getNumber() + 1,
+                page.getSize(),
                 page.getContent()
+        );
+    }
+
+    public static <T> PageResponse<T> of(PagedModel<T> pagedModel) {
+        return new PageResponse<>(
+                Long.valueOf(pagedModel.getMetadata().totalPages()).intValue(),
+                Long.valueOf(pagedModel.getMetadata().number() + 1).intValue(),
+                Long.valueOf(pagedModel.getMetadata().size()).intValue(),
+                pagedModel.getContent()
         );
     }
 }
