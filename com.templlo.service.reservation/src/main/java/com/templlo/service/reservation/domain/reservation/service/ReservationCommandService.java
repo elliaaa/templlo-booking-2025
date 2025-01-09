9a82bae766b714dbda9f5c49fc93be8c358297df
jeeheaG <jeehea99@naver.com>
2025-01-09 16:30:50 +0900

    :recycle: refactor : extract method of send kafka message

--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/service/ReservationCommandService.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/service/ReservationCommandService.java
@@ -5,13 +5,13 @@ import com.templlo.service.reservation.domain.reservation.controller.model.reque
 import com.templlo.service.reservation.domain.reservation.controller.model.response.CreateReservationRes;
 import com.templlo.service.reservation.domain.reservation.domain.Reservation;
 import com.templlo.service.reservation.domain.reservation.repository.ReservationRepository;
-import com.templlo.service.reservation.domain.reservation.service.model.CreateReservationMessage;
+import com.templlo.service.reservation.domain.reservation.service.model.produce.CreateReservationProduce;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.templlo.service.reservation.global.GlobalConst.TOPIC_CREATE_RESERVATION;
+import static com.templlo.service.reservation.domain.reservation.service.model.produce.ProducerTopic.TOPIC_CREATE_RESERVATION;
 
 @Service
 @Transactional(readOnly = false)
@@ -25,17 +25,19 @@ public class ReservationCommandService {
     public CreateReservationRes createReservation(CreateReservationReq requestDto, String userId) {
         // TODO : 사용자 id 비교 검증
 
-        // TODO : 예약 받는 시간 중인지 check?
-
         // save
         Reservation reservation = requestDto.toEntity();
         Reservation savedReservation = reservationRepository.save(reservation);
 
         // 예약 신청 이벤트 발행
-        CreateReservationMessage createReservationMessage = CreateReservationMessage.from(savedReservation, 1);
-
-        kafkaTemplate.send(TOPIC_CREATE_RESERVATION, null, gson.toJson(createReservationMessage));
+        produceReservationCreatedMessage(savedReservation, 0);
 
         return CreateReservationRes.from(savedReservation);
+    }
+
+
+    private void produceReservationCreatedMessage(Reservation savedReservation, int amount) {
+        CreateReservationProduce createReservationMessage = CreateReservationProduce.from(savedReservation, amount);
+        kafkaTemplate.send(TOPIC_CREATE_RESERVATION, null, gson.toJson(createReservationMessage));
     }
 }
