00875ea2938c0f7381f9e86a71a3b0b0bc27c70c
jeeheaG <jeehea99@naver.com>
2025-01-09 16:34:56 +0900

    :memo: chore : ake temp reservation event producer for test consumer

--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/temp/KafkaTestController.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/temp/KafkaTestController.java
@@ -1,6 +1,8 @@
 package com.templlo.service.reservation.domain.temp;
 
 import com.google.gson.Gson;
+import com.templlo.service.reservation.domain.reservation.service.model.consume.CreateReservationResultConsume;
+import com.templlo.service.reservation.domain.reservation.service.model.consume.CreateReservationResultStatus;
 import com.templlo.service.reservation.global.common.response.ApiResponse;
 import com.templlo.service.reservation.global.common.response.BasicStatusCode;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +10,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
+import static com.templlo.service.reservation.domain.reservation.service.model.consume.ConsumerTopicGroupName.TOPIC_RESERVATION_CONFIRM;
+
 @RestController
 @RequestMapping("api/test/kafka")
 @RequiredArgsConstructor
 public class KafkaTestController {
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final Gson gson;
+
+    @GetMapping("/create-reservation-result")
+    public ResponseEntity<ApiResponse> tempCreateReservationMessageProducer(@RequestParam UUID reservationId) {
+        CreateReservationResultConsume dto1 = CreateReservationResultConsume.builder()
+                .reservationId(reservationId)
+                .status(CreateReservationResultStatus.SUCCESS)
+                .build();
+
+        CreateReservationResultConsume dto2 = CreateReservationResultConsume.builder()
+                .reservationId(reservationId)
+                .status(CreateReservationResultStatus.FAILED)
+                .build();
+
+        kafkaTemplate.send(TOPIC_RESERVATION_CONFIRM, null, gson.toJson(dto1));
+        kafkaTemplate.send(TOPIC_RESERVATION_CONFIRM, null, gson.toJson(dto2));
+
+        return ResponseEntity.ok().body(ApiResponse.of(BasicStatusCode.OK));
+    }
 
     @GetMapping("/t1")
     public ResponseEntity<ApiResponse> produceMessage() {
