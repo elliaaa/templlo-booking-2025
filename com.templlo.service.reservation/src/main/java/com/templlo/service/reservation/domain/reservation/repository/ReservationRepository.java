b2b7b4d03b45b8c00c7791fcbdd6f1379d9724f8
jeeheaG <jeehea99@naver.com>
2025-01-10 03:08:45 +0900

    :sparkles: add : make repository methods for query reservations

--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/repository/ReservationRepository.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/repository/ReservationRepository.java
@@ -7,14 +7,29 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.web.PagedModel;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
     Page<Reservation> findAllByUserId(UUID userId, Pageable pageable);
 
+    Page<Reservation> findAllByProgramIdIn(List<UUID> programIds, Pageable pageable);
+
     default PagedModel<ReservationListRes> findAllByUserIdOfPagedModel(UUID userId, Pageable pageable) {
         Page<Reservation> pageEntity = findAllByUserId(userId, pageable);
+        return toReservationListResPagedModel(pageEntity);
+    }
+
+    default PagedModel<ReservationListRes> findAllByByProgramIdOfPagedModel(List<UUID> programIds, Pageable pageable) {
+        Page<Reservation> pageEntity = findAllByProgramIdIn(programIds, pageable);
+        return toReservationListResPagedModel(pageEntity);
+    }
+
+    private static PagedModel<ReservationListRes> toReservationListResPagedModel(Page<Reservation> pageEntity) {
         Page<ReservationListRes> pageDto = pageEntity.map(ReservationListRes::from);
         return new PagedModel<>(pageDto);
     }
+
+
+
 }
