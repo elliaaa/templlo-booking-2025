19f90e7dca296fff092f88aebe0a8951931b1b1d
ellia <yunjc536147@gmail.com>
2025-01-10 16:23:10 +0900

    :sparkles: [ edit ] ci fail로 인한 코드수정

--- com.templlo.service.temple/src/test/java/com/templlo/service/temple/TempleApplicationTests.java
+++ com.templlo.service.temple/src/test/java/com/templlo/service/temple/TempleApplicationTests.java
@@ -3,7 +3,6 @@ package com.templlo.service.temple;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TempleApplicationTests {
 
 	@Test
