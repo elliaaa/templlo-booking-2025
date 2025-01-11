b6485915bc9d38022bf4b852ea790d1c55151e7a
jeeheaG <jeehea99@naver.com>
2025-01-11 18:48:26 +0900

    :sparkles: feat : make annotation of adding feign client auth header by interceptor

--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/client/TempleClient.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/client/TempleClient.java
@@ -2,6 +2,7 @@ package com.templlo.service.reservation.domain.reservation.client;
 
 import com.templlo.service.reservation.domain.reservation.client.model.response.GetProgramsByTempleRes;
 import com.templlo.service.reservation.domain.reservation.client.model.response.ProgramServiceWrapperRes;
+import com.templlo.service.reservation.global.feign.AuthHeader;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,8 @@ import java.util.UUID;
 
 @FeignClient("program-service")
 public interface TempleClient {
+
+    @AuthHeader
     @GetMapping("/api/programs/temples/{templeId}")
     ProgramServiceWrapperRes<List<GetProgramsByTempleRes>> getProgramsByTemple(@PathVariable(name = "templeId") UUID templeId);
 }
---
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/global/feign/AuthHeader.java
@@ -0,0 +1,11 @@
+package com.templlo.service.reservation.global.feign;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthHeader {
+}
