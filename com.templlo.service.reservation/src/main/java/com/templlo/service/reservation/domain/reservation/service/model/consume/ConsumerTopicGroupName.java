88dc2568e9b51db10d405f10249aa0598eda6164
hyeririjeon <jeonhyeri5555@gmail.com>
2025-01-12 15:37:09 +0900

    :hammer: chore: topic name confirm -> confirmed

--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/service/model/consume/ConsumerTopicGroupName.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/service/model/consume/ConsumerTopicGroupName.java
@@ -1,7 +1,7 @@
 package com.templlo.service.reservation.domain.reservation.service.model.consume;
 
 public class ConsumerTopicGroupName {
-    public static final String TOPIC_RESERVATION_CONFIRM = "reservation-confirm";
+    public static final String TOPIC_RESERVATION_CONFIRM = "reservation-confirmed";
     public static final String GROUP_RESERVATION_CONFIRM = "reservation-confirm-reservation";
 
 }
