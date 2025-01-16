8d780bf8d59c46bf67e658fcac54916ed12b3742
jeeheaG <jeehea99@naver.com>
2025-01-17 02:45:57 +0900

    :sparkles: add : add key name

--- com.templlo.service.program/src/main/java/com/templlo/service/program/global/aop/distributed_lock/DistributedLockKey.java
+++ com.templlo.service.program/src/main/java/com/templlo/service/program/global/aop/distributed_lock/DistributedLockKey.java
@@ -1,5 +1,8 @@
 package com.templlo.service.program.global.aop.distributed_lock;
 
+import lombok.Getter;
+
+@Getter
 public enum DistributedLockKey {
     TEMPLE_STAY_PROGRAM_CAPACITY_PREFIX("temple-stay:capacity:");
 
