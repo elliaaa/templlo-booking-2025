ee234ad7182dcc472e9c6551a2b1eeebfa7431c1
jeeheaG <jeehea99@naver.com>
2025-01-24 12:12:15 +0900

    :fire: refactor : change BigDecimal to int or Integer

--- com.templlo.service.promotion/src/main/java/com/templlo/service/common/dto/DetailProgramResponse.java
+++ com.templlo.service.promotion/src/main/java/com/templlo/service/common/dto/DetailProgramResponse.java
@@ -1,13 +1,12 @@
 package com.templlo.service.common.dto;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record DetailProgramResponse(
 	UUID programId,
 	String type,
-	BigDecimal programFee,
+	int programFee,
 	LocalDate programDate
 ) {
 }
