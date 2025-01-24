d6571544e5dee4bfbe15793ef7dbadac68cb7317
Chang Yoon Ji <116702048+elliaaa@users.noreply.github.com>
2025-01-24 13:16:30 +0900

    [Feature/temple] 사찰 도메인 배포 파일 추가 (#93)
    * :sparkles: [ add ] 배포용 파일 추가
    
    * :bug: [ remove ] 서킷브레이커 코드 삭제
    
    * :bug: [ remove ] 서킷브레이커 코드 삭제
    
    * :bug: [ fix ] ECS 배포 세팅 수정사항 반영

---
+++ com.templlo.service.temple/src/main/java/com/templlo/service/temple/config/EcsConfig.java
@@ -0,0 +1,34 @@
+package com.templlo.service.temple.config;
+
+import com.netflix.appinfo.EurekaInstanceConfig;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Configuration
+@Profile("prod")
+public class EcsConfig {
+
+    @Bean
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        String ip = null;
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        config.setIpAddress(ip);
+        config.setPreferIpAddress(true);
+        config.setNonSecurePort(19020);
+
+
+        return config;
+    }
+}
