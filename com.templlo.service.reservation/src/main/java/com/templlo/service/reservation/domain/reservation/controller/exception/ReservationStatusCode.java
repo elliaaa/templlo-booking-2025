52b91fd8f089521616bf63b3f8728e019f7865d6
soo1e <gameswuon@gmail.com>
2025-01-26 22:54:54 +0900

    :sparkles: Status Code 추가

--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/controller/exception/ReservationStatusCode.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/controller/exception/ReservationStatusCode.java
@@ -1,32 +1,35 @@
 package com.templlo.service.reservation.domain.reservation.controller.exception;
 
+import org.springframework.http.HttpStatus;
+
 import com.templlo.service.reservation.global.common.response.StatusCode;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ReservationStatusCode implements StatusCode {
-    CREATE_RESERVATION_SUCCESS(HttpStatus.CREATED, "예약 신청에 성공했습니다"),
+	CREATE_RESERVATION_SUCCESS(HttpStatus.CREATED, "예약 신청에 성공했습니다"),
 
-    GET_RESERVATION_SUCCESS(HttpStatus.OK, "예약 단건 조회에 성공했습니다"),
+	GET_RESERVATION_SUCCESS(HttpStatus.OK, "예약 단건 조회에 성공했습니다"),
 
-    GET_RESERVATIONS_OF_USER_SUCCESS(HttpStatus.OK, "나의 예약 신청내역 조회에 성공했습니다"),
-    GET_RESERVATIONS_OF_TEMPLE_SUCCESS(HttpStatus.OK, "사찰의 예약 수신내역 조회에 성공했습니다"),
-    SEARCH_RESERVATIONS_SUCCESS(HttpStatus.OK, "예약 검색에 성공했습니다."),
+	GET_RESERVATIONS_OF_USER_SUCCESS(HttpStatus.OK, "나의 예약 신청내역 조회에 성공했습니다"),
+	GET_RESERVATIONS_OF_TEMPLE_SUCCESS(HttpStatus.OK, "사찰의 예약 수신내역 조회에 성공했습니다"),
+	SEARCH_RESERVATIONS_SUCCESS(HttpStatus.OK, "예약 검색에 성공했습니다."),
 
-    CANCEL_RESERVATION_SUCCESS(HttpStatus.OK, "예약 취소 신청에 성공했습니다"),
-    REJECT_RESERVATION_SUCCESS(HttpStatus.OK, "예약 거절 신청에 성공했습니다"),
+	CANCEL_RESERVATION_SUCCESS(HttpStatus.OK, "예약 취소 신청에 성공했습니다"),
+	REJECT_RESERVATION_SUCCESS(HttpStatus.OK, "예약 거절 신청에 성공했습니다"),
 
-    RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 id 의 예약을 찾을 수 없습니다"),
-    NOT_TEMPLE_OWNER(HttpStatus.FORBIDDEN, "해당 사찰에 대한 권한이 부족합니다");
+	RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 id 의 예약을 찾을 수 없습니다"),
+	NOT_TEMPLE_OWNER(HttpStatus.FORBIDDEN, "해당 사찰에 대한 권한이 부족합니다"),
+	COUPON_RESET_FAILED(HttpStatus.NOT_FOUND, "쿠폰 처리에 실패하였습니다");
 
-    private final HttpStatus httpStatus;
-    private final String message;
+	private final HttpStatus httpStatus;
+	private final String message;
 
-    @Override
-    public String getName() {
-        return this.name();
-    }
+	@Override
+	public String getName() {
+		return this.name();
+	}
 }
