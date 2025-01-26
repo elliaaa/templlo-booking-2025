7f326c9f2652196f2105380da0b613fefaecb025
soo1e <gameswuon@gmail.com>
2025-01-26 22:55:09 +0900

    :sparkles: Service 수정

--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/service/ReservationCommandService.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/service/ReservationCommandService.java
@@ -1,5 +1,15 @@
 package com.templlo.service.reservation.domain.reservation.service;
 
+import static com.templlo.service.reservation.domain.reservation.service.model.produce.ProducerTopic.*;
+
+import java.util.UUID;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.google.gson.Gson;
 import com.templlo.service.reservation.domain.reservation.client.ProgramClient;
 import com.templlo.service.reservation.domain.reservation.client.PromotionClient;
@@ -7,8 +17,8 @@ import com.templlo.service.reservation.domain.reservation.client.TempleClient;
 import com.templlo.service.reservation.domain.reservation.client.UserClient;
 import com.templlo.service.reservation.domain.reservation.client.model.request.UseCouponReq;
 import com.templlo.service.reservation.domain.reservation.client.model.response.DetailProgramRes;
-import com.templlo.service.reservation.domain.reservation.client.model.response.wrapper.ProgramServiceWrapperRes;
 import com.templlo.service.reservation.domain.reservation.client.model.response.UseCouponRes;
+import com.templlo.service.reservation.domain.reservation.client.model.response.wrapper.ProgramServiceWrapperRes;
 import com.templlo.service.reservation.domain.reservation.controller.exception.ReservationException;
 import com.templlo.service.reservation.domain.reservation.controller.exception.ReservationStatusCode;
 import com.templlo.service.reservation.domain.reservation.controller.model.request.CouponUsedType;
@@ -24,17 +34,8 @@ import com.templlo.service.reservation.domain.reservation.service.model.produce.
 import com.templlo.service.reservation.global.common.exception.BaseException;
 import com.templlo.service.reservation.global.common.response.BasicStatusCode;
 import com.templlo.service.reservation.global.security.UserRole;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.UUID;
-
-import static com.templlo.service.reservation.domain.reservation.service.model.produce.ProducerTopic.TOPIC_CANCEL_RESERVATION;
-import static com.templlo.service.reservation.domain.reservation.service.model.produce.ProducerTopic.TOPIC_CREATE_RESERVATION;
 
 // TODO : Transactional outbox 패턴 적용하고 save() 사용 안함
 
@@ -42,128 +43,134 @@ import static com.templlo.service.reservation.domain.reservation.service.model.p
 @Transactional(readOnly = false)
 @RequiredArgsConstructor
 public class ReservationCommandService {
-    private final ReservationRepository reservationRepository;
-    private final KafkaTemplate<String, String> kafkaTemplate;
-    private final UserClient userClient;
-    private final PromotionClient promotionClient;
-    private final Gson gson; // TODO : KafkaTemplate 새로 만들어서 처리하기
-    private final ProgramClient programClient;
-    private final TempleClient templeClient;
+	private final ReservationRepository reservationRepository;
+	private final KafkaTemplate<String, String> kafkaTemplate;
+	private final UserClient userClient;
+	private final PromotionClient promotionClient;
+	private final Gson gson; // TODO : KafkaTemplate 새로 만들어서 처리하기
+	private final ProgramClient programClient;
+	private final TempleClient templeClient;
 
-    // TODO : 함수의 책임, 트랜잭션에 대해 생각
-    public CreateReservationRes createReservation(CreateReservationReq requestDto, String userId) {
-        // TODO : 사용자 id 비교 검증
+	// TODO : 함수의 책임, 트랜잭션에 대해 생각
+	public CreateReservationRes createReservation(CreateReservationReq requestDto, String userId) {
+		// TODO : 사용자 id 비교 검증
 
-        // 프로그램 조회 - 가격 화인
-        DetailProgramRes detailProgramRes = getDetailProgramResponse(requestDto);
+		// 프로그램 조회 - 가격 화인
+		DetailProgramRes detailProgramRes = getDetailProgramResponse(requestDto);
 
-        // 쿠폰 사용 후 최종 금액 계산
-        int paymentAmount = useCouponAndGetPaymentAmount(requestDto, detailProgramRes);
+		// 쿠폰 사용 후 최종 금액 계산
+		int paymentAmount = useCouponAndGetPaymentAmount(requestDto, detailProgramRes);
 
-        // save
-        Reservation reservation = requestDto.toEntity(paymentAmount);
-        Reservation savedReservation = reservationRepository.save(reservation);
+		// save
+		Reservation reservation = requestDto.toEntity(paymentAmount);
 
-        // 예약 신청 이벤트 발행
-        produceReservationCreatedMessage(savedReservation, 0, ReservationOpenType.ADDITIONAL_OPEN);
+		if (isCouponUsed(requestDto)) {
+			reservation = reservation.withCouponId(requestDto.couponId());
+		}
+		Reservation savedReservation = reservationRepository.save(reservation);
 
-        return CreateReservationRes.from(savedReservation);
-    }
+		// 예약 신청 이벤트 발행
+		produceReservationCreatedMessage(savedReservation, 0, ReservationOpenType.ADDITIONAL_OPEN);
 
-    private int useCouponAndGetPaymentAmount(CreateReservationReq requestDto, DetailProgramRes detailProgramRes) {
-        if(isCouponUsed(requestDto)) {
-            UseCouponRes useCouponRes = promotionClient.useCouponAndGetFinalPrice(requestDto.couponId(), UseCouponReq.toDto(detailProgramRes, requestDto.programDate()));
-            return useCouponRes.finalPrice();
-        }
-        return detailProgramRes.programFee();
-    }
+		return CreateReservationRes.from(savedReservation);
+	}
 
-    private boolean isCouponUsed(CreateReservationReq requestDto) {
-        return requestDto.couponUsedType().equals(CouponUsedType.USED) && requestDto.couponId() != null;
-    }
+	private int useCouponAndGetPaymentAmount(CreateReservationReq requestDto, DetailProgramRes detailProgramRes) {
+		if (isCouponUsed(requestDto)) {
+			UseCouponRes useCouponRes = promotionClient.useCouponAndGetFinalPrice(requestDto.couponId(),
+				UseCouponReq.toDto(detailProgramRes, requestDto.programDate()));
+			return useCouponRes.finalPrice();
+		}
+		return detailProgramRes.programFee();
+	}
 
-    private DetailProgramRes getDetailProgramResponse(CreateReservationReq requestDto) {
-        ProgramServiceWrapperRes<DetailProgramRes> programResponse = programClient.getProgram(requestDto.programId());
-        return programResponse.data();
-    }
+	private boolean isCouponUsed(CreateReservationReq requestDto) {
+		return requestDto.couponUsedType().equals(CouponUsedType.USED) && requestDto.couponId() != null;
+	}
 
-    public CancelReservationRes cancelReservation(UUID reservationId, String userId) {
-        Reservation reservation = getReservation(reservationId);
-        checkCancelUser(userId, reservation);
+	private DetailProgramRes getDetailProgramResponse(CreateReservationReq requestDto) {
+		ProgramServiceWrapperRes<DetailProgramRes> programResponse = programClient.getProgram(requestDto.programId());
+		return programResponse.data();
+	}
 
-        reservation.updateStatusProcessingCancel();
-        Reservation savedReservation = reservationRepository.save(reservation);
+	public CancelReservationRes cancelReservation(UUID reservationId, String userId) {
+		Reservation reservation = getReservation(reservationId);
+		checkCancelUser(userId, reservation);
 
-        produceReservationCancelMessage(reservation, 0, ReservationOpenType.ADDITIONAL_OPEN);
+		reservation.updateStatusProcessingCancel();
+		Reservation savedReservation = reservationRepository.save(reservation);
 
-        return CancelReservationRes.from(savedReservation);
-    }
+		produceReservationCancelMessage(reservation, 0, ReservationOpenType.ADDITIONAL_OPEN);
 
-    public RejectReservationRes rejectReservation(UUID reservationId, String userId) {
-        Reservation reservation = getReservation(reservationId);
+		return CancelReservationRes.from(savedReservation);
+	}
 
-        checkRoleForTemple(reservation);
+	public RejectReservationRes rejectReservation(UUID reservationId, String userId) {
+		Reservation reservation = getReservation(reservationId);
 
-        reservation.updateStatusProcessingReject();
-        Reservation savedReservation = reservationRepository.save(reservation);
+		checkRoleForTemple(reservation);
 
-        produceReservationCancelMessage(reservation, 0, ReservationOpenType.ADDITIONAL_OPEN);
+		reservation.updateStatusProcessingReject();
+		Reservation savedReservation = reservationRepository.save(reservation);
 
-        return RejectReservationRes.from(savedReservation);
-    }
+		produceReservationCancelMessage(reservation, 0, ReservationOpenType.ADDITIONAL_OPEN);
 
-    private void checkRoleForTemple(Reservation reservation) {
-        String role = getRole();
-        if(role.equals(UserRole.TEMPLE_ADMIN.name())) {
-            UUID templeId = getTempleOfProgram(reservation.getProgramId());
-            checkTempleOwnership(templeId);
-        }
-        // MASTER 면 그냥 허용
-    }
+		return RejectReservationRes.from(savedReservation);
+	}
 
-    private String getRole() {
-        return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .findFirst()
-                .orElseThrow();
-    }
+	private void checkRoleForTemple(Reservation reservation) {
+		String role = getRole();
+		if (role.equals(UserRole.TEMPLE_ADMIN.name())) {
+			UUID templeId = getTempleOfProgram(reservation.getProgramId());
+			checkTempleOwnership(templeId);
+		}
+		// MASTER 면 그냥 허용
+	}
 
-    private void checkTempleOwnership(UUID templeId) {
-        try{
-            templeClient.checkTempleOwnership(templeId);
-        } catch (Exception e) {
-            throw new ReservationException(ReservationStatusCode.NOT_TEMPLE_OWNER);
-        }
-    }
+	private String getRole() {
+		return SecurityContextHolder.getContext().getAuthentication().getAuthorities().stream()
+			.map(GrantedAuthority::getAuthority)
+			.findFirst()
+			.orElseThrow();
+	}
 
-    private UUID getTempleOfProgram(UUID programId) {
-        ProgramServiceWrapperRes<DetailProgramRes> programRes = programClient.getProgram(programId);
-        DetailProgramRes detailProgramRes = programRes.data();
-        return detailProgramRes.templeId();
-    }
+	private void checkTempleOwnership(UUID templeId) {
+		try {
+			templeClient.checkTempleOwnership(templeId);
+		} catch (Exception e) {
+			throw new ReservationException(ReservationStatusCode.NOT_TEMPLE_OWNER);
+		}
+	}
 
+	private UUID getTempleOfProgram(UUID programId) {
+		ProgramServiceWrapperRes<DetailProgramRes> programRes = programClient.getProgram(programId);
+		DetailProgramRes detailProgramRes = programRes.data();
+		return detailProgramRes.templeId();
+	}
 
-    private void produceReservationCreatedMessage(Reservation savedReservation, int amount, ReservationOpenType openType) {
-        CreateReservationProduce createReservationMessage = CreateReservationProduce.from(savedReservation, amount, openType);
-        kafkaTemplate.send(TOPIC_CREATE_RESERVATION, null, gson.toJson(createReservationMessage));
-    }
+	private void produceReservationCreatedMessage(Reservation savedReservation, int amount,
+		ReservationOpenType openType) {
+		CreateReservationProduce createReservationMessage = CreateReservationProduce.from(savedReservation, amount,
+			openType);
+		kafkaTemplate.send(TOPIC_CREATE_RESERVATION, null, gson.toJson(createReservationMessage));
+	}
 
-    private void produceReservationCancelMessage(Reservation savedReservation, int amount, ReservationOpenType openType) {
-        CancelReservationProduce message = CancelReservationProduce.from(savedReservation, amount, openType);
-        kafkaTemplate.send(TOPIC_CANCEL_RESERVATION, null, gson.toJson(message));
-    }
+	private void produceReservationCancelMessage(Reservation savedReservation, int amount,
+		ReservationOpenType openType) {
+		CancelReservationProduce message = CancelReservationProduce.from(savedReservation, amount, openType);
+		kafkaTemplate.send(TOPIC_CANCEL_RESERVATION, null, gson.toJson(message));
+	}
 
+	private void checkCancelUser(String userId, Reservation reservation) {
+		UUID userUUID = userClient.getUser(userId).data().id();
 
-    private void checkCancelUser(String userId, Reservation reservation) {
-        UUID userUUID = userClient.getUser(userId).data().id();
+		if (!reservation.getUserId().equals(userUUID)) {
+			throw new BaseException(BasicStatusCode.FORBIDDEN);
+		}
+	}
 
-        if(!reservation.getUserId().equals(userUUID)) {
-            throw new BaseException(BasicStatusCode.FORBIDDEN);
-        }
-    }
-
-    private Reservation getReservation(UUID reservationId) {
-        return reservationRepository.findById(reservationId)
-                .orElseThrow(() -> new ReservationException(ReservationStatusCode.RESERVATION_NOT_FOUND));
-    }
+	private Reservation getReservation(UUID reservationId) {
+		return reservationRepository.findById(reservationId)
+			.orElseThrow(() -> new ReservationException(ReservationStatusCode.RESERVATION_NOT_FOUND));
+	}
 }
--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/service/ReservationConsumeService.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/service/ReservationConsumeService.java
@@ -1,56 +1,74 @@
 package com.templlo.service.reservation.domain.reservation.service;
 
+import static com.templlo.service.reservation.domain.reservation.service.model.consume.ConsumerTopicGroupName.*;
+
+import java.util.UUID;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.google.gson.Gson;
+import com.templlo.service.reservation.domain.reservation.client.PromotionClient;
 import com.templlo.service.reservation.domain.reservation.controller.exception.ReservationException;
 import com.templlo.service.reservation.domain.reservation.controller.exception.ReservationStatusCode;
 import com.templlo.service.reservation.domain.reservation.domain.Reservation;
 import com.templlo.service.reservation.domain.reservation.repository.ReservationRepository;
 import com.templlo.service.reservation.domain.reservation.service.model.consume.CancelReservationResultConsume;
 import com.templlo.service.reservation.domain.reservation.service.model.consume.CreateReservationResultConsume;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.UUID;
-
-import static com.templlo.service.reservation.domain.reservation.service.model.consume.ConsumerTopicGroupName.*;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReservationConsumeService {
-    private final ReservationRepository reservationRepository;
-    private final Gson gson;
+	private final ReservationRepository reservationRepository;
+	private final PromotionClient promotionClient;
+	private final Gson gson;
 
-    @KafkaListener(groupId = GROUP_RESERVATION_CONFIRM, topics = TOPIC_RESERVATION_CONFIRM)
-    @Transactional
-    public void createReservationResultConsumer(String messageStr) {
-        CreateReservationResultConsume message = gson.fromJson(messageStr, CreateReservationResultConsume.class);
+	@KafkaListener(groupId = GROUP_RESERVATION_CONFIRM, topics = TOPIC_RESERVATION_CONFIRM)
+	@Transactional
+	public void createReservationResultConsumer(String messageStr) {
+		CreateReservationResultConsume message = gson.fromJson(messageStr, CreateReservationResultConsume.class);
 
-        Reservation reservation = getReservation(message.reservationId());
+		Reservation reservation = getReservation(message.reservationId());
 
-        switch (message.status()) {
-            case SUCCESS -> reservation.updateStatusCompleted();
-            case FAILURE -> reservation.updateStatusFailed();
-        }
-    }
+		switch (message.status()) {
+			case SUCCESS -> reservation.updateStatusCompleted();
+			case FAILURE -> {
+				reservation.updateStatusFailed();
 
-    @KafkaListener(groupId = GROUP_RESERVATION_CANCEL_CONFIRM, topics = TOPIC_RESERVATION_CANCEL_CONFIRM)
-    @Transactional
-    public void cancelReservationResultConsumer (String messageStr){
-        CancelReservationResultConsume message = gson.fromJson(messageStr, CancelReservationResultConsume.class);
+				if (reservation.getCouponId() != null) {
+					resetCouponStatus(reservation.getCouponId());
+				}
+			}
+		}
+	}
 
-        Reservation reservation = getReservation(message.reservationId());
+	@KafkaListener(groupId = GROUP_RESERVATION_CANCEL_CONFIRM, topics = TOPIC_RESERVATION_CANCEL_CONFIRM)
+	@Transactional
+	public void cancelReservationResultConsumer(String messageStr) {
+		CancelReservationResultConsume message = gson.fromJson(messageStr, CancelReservationResultConsume.class);
 
-        switch (message.status()) {
-            case SUCCESS -> reservation.updateStatusCanceledOrRejected();
-            case FAILURE -> reservation.updateStatusCanceledOrRejectedFailed();
-        }
-    }
+		Reservation reservation = getReservation(message.reservationId());
 
-    private Reservation getReservation(UUID message) {
-        return reservationRepository.findById(message).orElseThrow(
-                () -> new ReservationException(ReservationStatusCode.RESERVATION_NOT_FOUND));
-    }
+		switch (message.status()) {
+			case SUCCESS -> reservation.updateStatusCanceledOrRejected();
+			case FAILURE -> reservation.updateStatusCanceledOrRejectedFailed();
+		}
+	}
+
+	private Reservation getReservation(UUID message) {
+		return reservationRepository.findById(message).orElseThrow(
+			() -> new ReservationException(ReservationStatusCode.RESERVATION_NOT_FOUND));
+	}
+
+	private void resetCouponStatus(UUID couponId) {
+		try {
+			promotionClient.resetCoupon(couponId);
+		} catch (Exception e) {
+			throw new ReservationException(ReservationStatusCode.COUPON_RESET_FAILED);
+		}
+	}
 }
