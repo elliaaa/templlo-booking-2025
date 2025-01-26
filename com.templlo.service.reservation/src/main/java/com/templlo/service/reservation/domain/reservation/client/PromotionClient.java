3538d283c14574e474b093cab0b11bcf1e352d19
soo1e <gameswuon@gmail.com>
2025-01-26 22:56:00 +0900

    :sparkles: 쿠폰 리셋 호출

--- com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/client/PromotionClient.java
+++ com.templlo.service.reservation/src/main/java/com/templlo/service/reservation/domain/reservation/client/PromotionClient.java
@@ -1,22 +1,27 @@
 package com.templlo.service.reservation.domain.reservation.client;
 
-import com.templlo.service.reservation.domain.reservation.client.model.request.UseCouponReq;
-import com.templlo.service.reservation.domain.reservation.client.model.response.UseCouponRes;
-import com.templlo.service.reservation.global.feign.AuthHeader;
+import java.util.UUID;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.UUID;
-
+import com.templlo.service.reservation.domain.reservation.client.model.request.UseCouponReq;
+import com.templlo.service.reservation.domain.reservation.client.model.response.UseCouponRes;
+import com.templlo.service.reservation.global.feign.AuthHeader;
 
 @FeignClient("promotion-service")
 public interface PromotionClient {
 
-    @AuthHeader
-    @GetMapping("/api/coupons/{couponId}/use")
-    UseCouponRes useCouponAndGetFinalPrice(
-            @PathVariable(name = "couponId") UUID couponId,
-            @RequestBody UseCouponReq useCouponReq);
+	@AuthHeader
+	@GetMapping("/api/coupons/{couponId}/use")
+	UseCouponRes useCouponAndGetFinalPrice(
+		@PathVariable(name = "couponId") UUID couponId,
+		@RequestBody UseCouponReq useCouponReq);
+
+	@AuthHeader
+	@GetMapping("/api/coupons/{couponId}/reset")
+	void resetCoupon(@PathVariable(name = "couponId") UUID couponId);
 }
+
